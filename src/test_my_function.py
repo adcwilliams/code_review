f8129ecb0f2c5df964b8bc74675c8667688fa8d3
Andrew Williams <a.d.c.williams@bristol.ac.uk>
2019-08-30 14:39:01 +0100

    Break test to see if pipeline badge reports it

--- src/test_my_function.py
+++ src/test_my_function.py
@@ -6,4 +6,4 @@ def test_addition():
     """Test the add_integers() function."""
 
     # Choose known arguments, 13, and 7, which will return a result of 20.
-    assert my_function.add_integers(13, 7) == 20
+    assert my_function.add_integers(13, 7) == 21
