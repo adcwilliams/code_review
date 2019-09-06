5a32af9820c5022284c2b2e97385a72bfcef1b95
Andrew Williams <a.d.c.williams@bristol.ac.uk>
2019-09-06 10:53:00 +0100

    Added basic calc file ready for the issue to improve it

--- src/calc.py
+++ src/calc.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
+"""Calculator program intended to solve all your difficult arithmetical
+challenges!
 
-print("hej!")
+Written according to the PEP-8 Style Guide
+"""
+
+import argparse
+
+# Acquire two numbers from the command line
+PARSER = argparse.ArgumentParser(description='Grab some numbers')
+PARSER.add_argument('number', type=int, nargs=2,
+                    help='two numbers to be processed')
+
+ARGS = PARSER.parse_args()
+
+# Add the two numbers
+RESULT = ARGS.number[0] + ARGS.number[1]
+
+# Print out the result
+print("Answer: {:d}".format(RESULT))
